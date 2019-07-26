ee921fa5eb08339ef1dc7ab90550be28e13e74ce
Nicholas Newman <nick.newman95@yahoo.com>
2019-07-26 12:58:12 -0600

    Added KNN File

---
+++ Python_sklearn_KNN.py
@@ -0,0 +1,38 @@
+# Python tutorial using scikit-learn for k nearest neighbors (KNN) on wine dataset
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+# Import the wine dataset
+dataSet = datasets.load_wine()
+
+# Analyze the feature set of the data
+#print("features: ", dataSet.feature_names)
+
+# Analyze the target set of the data
+#print("Labels: ", dataSet.target_names)
+
+# Analyze the first five entires of the dataset's values
+#print(dataSet.data[0:5])
+
+# Analyze the target set of the data
+#print(dataSet.target)
+
+# Analyze the dataset's shape 
+#print(dataSet.data.shape)
+
+# Split the whole dataset into seperate training and testing dataset
+X_train, X_test, y_train, y_test = train_test_split(dataSet.data, dataSet.target, test_size=0.3)
+
+# Create the KNN model
+theModel = KNeighborsClassifier(n_neighbors=5)
+# Train the model using the training sets
+theModel.fit(X_train, y_train)
+# Predict the testing dataset using the recently created KNN model
+theModel_Predict = theModel.predict(X_test)
+
+# Using the sklearn metrics library analyze the models accuracy
+print("Accuracy: ", metrics.accuracy_score(y_test, theModel_Predict))
+
