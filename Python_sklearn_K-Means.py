895da88ae80e376eb3680398f0386867158e130a
Nicholas Newman <nick.newman95@yahoo.com>
2019-08-01 12:09:54 -0600

    Added K-Means File

---
+++ Python_sklearn_K-Means.py
@@ -0,0 +1,47 @@
+# Python tutorial using scikit-learn for k-means clustering using a randomly generated dataset
+
+# Import python libraries
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+
+# create randomly generated dataset containing points
+X, y = make_blobs(n_samples=150, n_features=2, centers=3, cluster_std=0.5, shuffle=True, random_state=0)
+# plot the newly generated dataset using matplotlib
+plt.scatter(X[:, 0], X[:, 1], c='white', marker='o', edgecolor='black', s=50)
+plt.show()
+
+# Create the K Means model
+km = KMeans(n_clusters=3, init='random', n_init=10, max_iter=300, tol=1e-04, random_state=0)
+
+#Train the model using the X dataset
+y_km = km.fit_predict(X)
+
+
+# plot the three seperate clusters using the scatter function
+plt.scatter(X[y_km == 0, 0], X[y_km == 0, 1], s=50, c='lightgreen', marker='s', edgecolor='black',label='cluster 1')
+
+plt.scatter(X[y_km == 1, 0], X[y_km == 1, 1], s=50, c='orange', marker='o', edgecolor='black',label='cluster 2')
+
+plt.scatter(X[y_km == 2, 0], X[y_km == 2, 1], s=50, c='lightblue', marker='v', edgecolor='black',label='cluster 3')
+
+# plot the three seperate centroids for each of the clusters
+plt.scatter(km.cluster_centers_[:, 0], km.cluster_centers_[:, 1], s=250, marker='*', c='red', edgecolor='black', label='centroids')
+plt.legend(scatterpoints=1)
+plt.grid()
+plt.show()
+
+#################### The elbow method ####################
+
+# calculate the distortion map for the range of number of clusters that should be using for K Means
+distortions = []
+for i in range(1, 11):
+    km = KMeans(n_clusters=i, init='random', n_init=10, max_iter=300, tol=1e-04, random_state=0)
+    km.fit(X)
+    distortions.append(km.inertia_)
+
+# plot the distortion map using matplotlib
+plt.plot(range(1, 11), distortions, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distortion')
+plt.show()
