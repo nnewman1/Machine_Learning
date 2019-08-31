ebf9963a2e71ada97c9dce0582a4cabe28c97553
Nicholas Newman <nick.newman95@yahoo.com>
2019-08-30 22:36:02 -0600

    Add files via upload

---
+++ Matlab_ForwardPropagation.m
@@ -0,0 +1,55 @@
+% Matlab tutorial for forward propagation used by ANN algorithms on a sudo dataset.
+
+clc;
+clear;
+
+% Create the X dataset
+x = [1 0.7 0.3]'; % input values
+% Create the Y dataset
+y = [1.0]'; % Output labels for prediction
+% Create size variables for the X & Y Dataset
+N_x = size(x,1); % X row size
+N_y = size(y,1); % Y row size
+% Create the number of nodes each hidden layer will have
+N_h0 = 3; % Hidden Layer 0 Nodes 
+N_h1 = 2; % Hidden Layer 1 Nodes
+
+% Initalize the weights for each hidden and output layer
+W_h0  = rand(N_h0,N_x); % Weights for hidden layer 0 
+N_h0r = size(W_h0,1); % Hidden Layer 0 row size
+W_h1 = rand(N_h1,N_h0r); % Weights for hidden layer 1
+N_h1r = size(W_h1,1); % Hidden Layer 1 row size
+W_y  = rand(N_y,N_h1r); % weights for output layer
+
+% Step one, calculate the sum of Hidden layer 0
+Z_h0 = W_h0 * x;
+Z_h0 = Z_h0';
+
+% Step two, calculate activation for hidden layer 0 
+h0 = Sigmoid(Z_h0);
+h0 = h0';
+
+% Step three, calculate the sum of hidden layer 1
+Z_h1 = W_h1 * h0;
+Z_h1 = Z_h1';
+
+% Step four, calculate activation for hidden layer 1
+h1 = Sigmoid(Z_h1);
+h1 = h1';
+
+% Step five, calculate the sum for the output layer
+Z_y = W_y * h1;
+Z_y = Z_y';
+
+% Step six, calculate activation for the output layer
+y_hat = Sigmoid(Z_y);
+y_hat = y_hat';
+
+% Step seven, calculate error between Y_hat and predicted labels Y
+error_y = (y-y_hat);
+
+
+
+
+
+
