3297e1becce67642bb083d9d10a9adefba6f04b6
Nicholas Newman <nick.newman95@yahoo.com>
2019-09-05 10:24:04 -0600

    Added numpy ANN short file

---
+++ Python_numpy_ANN_short.py
@@ -0,0 +1,24 @@
+# Short Python tutorial using numpy with a one hidden layer Artificial Neural Network (ANN) on a sudo generated dataset.
+
+# Import python libraries
+from numpy import exp, array, random, dot
+# Define training set inputs
+training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+# Define training set outputs
+training_set_outputs = array([[0, 1, 1, 0]]).T
+# Define the seed for reproducibility
+random.seed(1)
+# Define a single neuron, with 3 input connections and 1 output connection
+synaptic_weights = 2 * random.random((3, 1)) - 1
+# Anaylze the random starting synaptic weights
+print ("Random starting synaptic weights: \n", synaptic_weights, '\n')
+# Run the ANN model 10000 times
+for iteration in range(10000):
+	# Pass the training set through our neural network (a single neuron).
+    output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
+    # We train the neural network's weights through a process of trial and error.
+    synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
+# Anaylze the random synaptic weights after training
+print ("New synaptic weights after training: \n", synaptic_weights, '\n')
+# Test and anaylze the neural network with a new situation.
+print ("Considering new situation [1, 0, 0] -> Accuracy:", 1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights)))))
