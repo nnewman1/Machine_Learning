a7838739534a2f129358d12e3756c8168cd53b55
Nicholas Newman <nick.newman95@yahoo.com>
2019-09-16 23:01:48 -0600

    Update Python_numpy_ANN_Draw.py

--- Python_numpy_ANN_Draw.py
+++ Python_numpy_ANN_Draw.py
@@ -1,3 +1,7 @@
+# Python tutorial using numpy with a one hidden layer Artificial Neural Network (ANN) with plots on a sudo generated dataset.
+# An Artificial Neural Network is based on the structure of a biological brain. 
+# These systems learn to perform tasks or classify based on data, without the need to be programmed specific task rules.
+
 from matplotlib import pyplot
 from math import cos, sin, atan
 
