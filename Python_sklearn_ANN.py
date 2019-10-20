7e9dceb90e0209800e63862802922141d0ed9293
Nicholas Newman <nick.newman95@yahoo.com>
2019-10-19 22:33:23 -0600

    Update Python_sklearn_ANN.py

--- Python_sklearn_ANN.py
+++ Python_sklearn_ANN.py
@@ -1,6 +1,7 @@
 # Python tutorial using scikit-learn with a one hidden layer Artificial Neural Network (ANN) on the iris dataset.
 # An Artificial Neural Network is based on the structure of a biological brain. 
 # These systems learn to perform tasks or classify based on data, without the need to be programmed specific task rules.
+# sci-kit learn or sklearn is an high-level machine learning library for python.
 
 # Import python libraries
 from sklearn import datasets
