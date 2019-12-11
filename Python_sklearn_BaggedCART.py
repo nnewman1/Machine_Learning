a163c8f3e9df3f76504cfa5f23a79d6acf697de4
Nicholas Newman <nick.newman95@yahoo.com>
2019-12-10 23:03:50 -0700

    Update Python_sklearn_BaggedCART.py

--- Python_sklearn_BaggedCART.py
+++ Python_sklearn_BaggedCART.py
@@ -1,6 +1,7 @@
 # Python tutorial using scikit-learn with Bagged Decision Trees on the pima indians diabetes dataset
 # Bootstrap Aggregation (Bagging) involves taking multiple samples from your whole training dataset and then training sub-models (typically of the same type) from each sample
 # The final output prediction is averaged across the predictions of all of the sub-models.
+# Python is an interpreted, high-level, general-purpose programming language.
 # sci-kit learn or sklearn is an high-level machine learning library for python.
 
 #Import python libraries
