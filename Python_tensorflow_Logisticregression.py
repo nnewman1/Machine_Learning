1032046ef2e7fb21a650664c7a91c0367bfa32a2
Nicholas Newman <nick.newman95@yahoo.com>
2020-02-07 22:01:16 -0700

    Update Python_tensorflow_Logisticregression.py

--- Python_tensorflow_Logisticregression.py
+++ Python_tensorflow_Logisticregression.py
@@ -3,6 +3,7 @@
 # Python is an interpreted, high-level, general-purpose programming language.
 # Tensorflow is an open source machine learning platform for python.
 # NumPy is a library for the Python programming language, adding support for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions to operate on these arrays.
+# sci-kit learn or sklearn is an high-level machine learning library for python.
 
 # Import Python Libraries
 import numpy as np
